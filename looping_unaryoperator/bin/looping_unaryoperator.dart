747b89ff838a894c4c7b346fe3b61faeac4eda05
Putri Mila <putrimiila.fx@gmail.com>
2024-10-13 19:55:14 +0800

    add

---
+++ looping_unaryoperator/bin/looping_unaryoperator.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  // looping dart
+  // for (int counter = 9; counter > 1; counter -= 2) {
+  //   print("halo");
+  // }
+
+  // while
+  // int i = 0;
+  // while (i < 5) {
+  //   print("halo ke $i");
+  //   i++;
+  // }
+
+  // print("=====Do While=========");
+
+  // int x = 0;
+  // do {
+  //   print("dododo ke $x");
+  //   x += 1;
+  // } while (x < 5);
+
+  //Operator Unary ++/--
+  int a, b;
+  a = 10;
+  b = a++;
+  print(a.toString() + " - " + b.toString());
+}
